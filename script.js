2b54548cc68d8490c6398a3b82ff3b09ac505734
Daniel <93330784+daniwth@users.noreply.github.com>
2023-09-07 15:53:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,27 @@
-let operando1 = '';
-let operando2 = '';
-let operador = '';
-let resultado = '';
+let display = document.getElementById('display');
+let expression = '';
 
-function agregarNumero(numero) {
-    if (operador === '') {
-        operando1 += numero;
-    } else {
-        operando2 += numero;
-    }
-    actualizarResultado();
+function appendCharacter(character) {
+  expression += character;
+  display.value = expression;
 }
 
-function agregarOperador(op) {
-    if (operando1 !== '' && operando2 !== '') {
-        calcular();
-    }
-    operador = op;
+function clearDisplay() {
+  expression = '';
+  display.value = '';
 }
 
-function agregarDecimal() {
-    if (operador === '') {
-        if (!operando1.includes('.')) {
-            operando1 += '.';
-        }
-    } else {
-        if (!operando2.includes('.')) {
-            operando2 += '.';
-        }
-    }
-    actualizarResultado();
+function deleteLastCharacter() {
+  expression = expression.slice(0, -1);
+  display.value = expression;
 }
 
-function calcular() {
-    const num1 = parseFloat(operando1);
-    const num2 = parseFloat(operando2);
-    switch (operador) {
-        case '+':
-            resultado = num1 + num2;
-            break;
-        case '-':
-            resultado = num1 - num2;
-            break;
-        case '*':
-            resultado = num1 * num2;
-            break;
-        case '/':
-            resultado = num1 / num2;
-            break;
-        default:
-            break;
-    }
-    operando1 = resultado.toString();
-    operando2 = '';
-    operador = '';
-    actualizarResultado();
-}
-
-function limpiar() {
-    operando1 = '';
-    operando2 = '';
-    operador = '';
-    resultado = '';
-    actualizarResultado();
-}
-
-function borrar() {
-    if (operador === '') {
-        operando1 = operando1.slice(0, -1);
-    } else {
-        operando2 = operando2.slice(0, -1);
-    }
-    actualizarResultado();
-}
-
-function actualizarResultado() {
-    document.getElementById('resultado').value = operando1 + operador + operando2;
+function calculateResult() {
+  try {
+    let result = eval(expression);
+    expression = result.toString();
+    display.value = expression;
+  } catch (error) {
+    display.value = 'Error';
+  }
 }
