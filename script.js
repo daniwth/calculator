4e59c524d2b4112c61f4fe6f0297bff4f4ae71e9
Daniel <93330784+daniwth@users.noreply.github.com>
2023-09-07 16:08:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-// Lógica de la calculadora
 const display = document.getElementById('result');
 const buttons = document.querySelectorAll('.number, .operator, .equal');
 
 buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        if (button.classList.contains('equal')) {
-            calculate();
-        } else {
-            display.value += button.textContent;
-        }
-    });
+  button.addEventListener('click', () => {
+    display.value += button.textContent;
+  });
 });
 
-function calculate() {
-    try {
-        const result = eval(display.value);
-        display.value = result;
-    } catch (error) {
-        display.value = 'Error';
-    }
-}
+document.querySelector('.equal').addEventListener('click', () => {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+});
